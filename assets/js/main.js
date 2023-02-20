68ef062bb0dabf8cd205ae133f1fb00ec6f2134e
Jeong-Wan Choi <97984552+choiqz@users.noreply.github.com>
2023-02-20 12:28:05 -0500

    update website

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,20 +1,19 @@
 /*========= MENU SHOW Y HIDDEN ==========*/
 const navMenu = document.getElementById('nav-menu'),
-	navToggle = document.getElementById('nav-toggle'),
-	navClose = document.getElementById('nav-close')
+	  navToggle = document.getElementById('nav-toggle'),
+	  navClose = document.getElementById('nav-close')
 
 /*========= MENU SHOW ==========*/
 /* Validate if constant exists */
-if (navToggle) {
+if(navToggle){
 	navToggle.addEventListener('click', () =>{
 		navMenu.classList.add('show-menu')
 	})	
-	
 }
 	
 /*========= MENU HIDDEN ==========*/
 /* Validate if constant exists */
-if (navClose) {
+if(navClose){
 	navClose.addEventListener('click', () =>{
 		navMenu.classList.remove('show-menu')
 	})
