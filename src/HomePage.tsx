cbeaf51b12ca3fbbb04fa7407b174a2fd01ac63c
Jeong-Wan Choi <jeong-wan.choi@bc.edu>
2023-11-14 16:01:10 -0500

    add resume section

--- src/HomePage.tsx
+++ src/HomePage.tsx
@@ -10,9 +10,17 @@ interface HomePageProps {
 const HomePage: React.FC<HomePageProps> = ({ name, tagline }) => {
   return (
     <div className="homepage">
-      <div className="profile-section">
-        <img src={process.env.PUBLIC_URL + '/about_updated.jpg'} alt={name} className="profile-image" />
-      </div>
+	  <div className="left-section">
+		  <div className="profile-section">
+			<img src={process.env.PUBLIC_URL + '/about_updated.jpg'} alt={name} className="profile-image" />
+		  </div>
+		  <div className="resume-section">
+			<a href="https://github.com/choiqz/resume" target="_blank" rel="noopener noreferrer" className="resume-link">
+				<h2 className="resume-header">Resume</h2>
+			</a>
+			<iframe src={process.env.PUBLIC_URL + '/Jeong-Wan_Choi_Resume.pdf'} title="Resume" className="resume-pdf"></iframe>
+		  </div>
+	  </div>
       <div className="intro-section">
         <h2>Hello! 👋</h2>
         <p>{tagline}</p>
