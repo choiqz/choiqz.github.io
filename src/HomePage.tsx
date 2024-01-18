19bd629199fc14002ad59e8f9959622e65c5ec17
Jeong-Wan Choi <jeong-wan.choi@bc.edu>
2024-01-18 00:24:07 -0500

    remove resume section and add hand wave animation

--- src/HomePage.tsx
+++ src/HomePage.tsx
@@ -1,4 +1,5 @@
 
+
 import React from 'react';
 import './HomePage.css';
 
@@ -10,19 +11,12 @@ interface HomePageProps {
 const HomePage: React.FC<HomePageProps> = ({ name, tagline }) => {
   return (
     <div className="homepage">
-	  <div className="left-section">
 		  <div className="profile-section">
 			<img src={process.env.PUBLIC_URL + '/about_updated.jpg'} alt={name} className="profile-image" />
 		  </div>
-		  <div className="resume-section">
-			<a href="https://github.com/choiqz/resume" target="_blank" rel="noopener noreferrer" className="resume-link">
-				<h2 className="resume-header">Resume</h2>
-			</a>
-			<iframe src={process.env.PUBLIC_URL + '/Jeong-Wan_Choi_Resume.pdf'} title="Resume" className="resume-pdf"></iframe>
-		  </div>
-	  </div>
-      <div className="intro-section">
-        <h2>Hello! 👋</h2>
+
+	  <div className="intro-section">
+	  <h2>Hello! <span className="wave-emoji">👋</span></h2>
         <p>{tagline}</p>
 		<div className="social-icons">
           <a href="https://github.com/choiqz" target="_blank" rel="noopener noreferrer">
@@ -38,4 +32,3 @@ const HomePage: React.FC<HomePageProps> = ({ name, tagline }) => {
 };
 
 export default HomePage;
-
